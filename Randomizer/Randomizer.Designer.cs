f69193b9dc74bbad040ec2f19b7b318f6477d1a8
Oksana Luzhniak <luzhnyako@gmail.com>
2024-04-14 15:27:25 +0300

    Перший коміт

---
+++ Randomizer/Randomizer.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Randomizer
+{
+    partial class Randomizer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введіть прізвище студента";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(365, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введіть ім\'я студента";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 80);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 40);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(368, 80);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 40);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(225, 230);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 40);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(186, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(272, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Введіть номер ЛР для генерації варіанту";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Salmon;
+            this.button1.Location = new System.Drawing.Point(133, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Закрити";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.Location = new System.Drawing.Point(368, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Згенерувати";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Randomizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(628, 462);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Randomizer";
+            this.Text = "Randomizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Randomizer/Randomizer.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using text = DocumentFormat.OpenXml.Wordprocessing.Text;
+
+
+namespace Randomizer
+{
+    public partial class Randomizer : Form
+    {
+        public Randomizer()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fullName = (string)textBox1.Text + " " + (string)textBox2.Text;
+            
+            // Перевірка, чи є студент з введеним прізвищем
+            string studentsExcelFilePath = "D:\\Diplom\\StudentsList.xlsx"; // Шлях до файлу зі списком студентів
+            bool studentExists = CheckStudentExists(fullName, studentsExcelFilePath);
+            if (studentExists)
+            {
+                int labNumber = int.Parse(textBox3.Text);
+
+                // Визначення, чи потрібні студенту складні завдання
+                bool isComplexTasks = IsComplexTasks(fullName, studentsExcelFilePath);
+
+                // Вибір файлу з завданнями в залежності від складності завдань
+                string templateFilePath;
+                if (labNumber==1)
+                {
+                    // Заміна поля {StudentName} на значення studentName
+                     templateFilePath = "D:\\Diplom\\Template1.docx";
+                }
+                else if (labNumber == 2)
+                {
+                    // Заміна поля {StudentName} на значення studentName
+                    templateFilePath = "D:\\Diplom\\Template2.docx";
+                }
+                else
+                {
+                    // Заміна поля {StudentName} на значення studentName
+                    templateFilePath = "D:\\Diplom\\Template3.docx";
+                }
+                string tasksTextFilePath;
+                if (isComplexTasks)
+                    tasksTextFilePath = "D:\\Diplom\\АтрибутиСкладні.txt"; // Шлях до файлу зі складними завданнями
+                else
+                    tasksTextFilePath = "D:\\Diplom\\АтрибутиПрості.txt"; // Шлях до файлу зі простими завданнями
+
+                string [] tasks= SelectTaskForStudent(fullName, labNumber, tasksTextFilePath);
+                string newFilePath = InsertValuesIntoTemplate(templateFilePath, fullName, labNumber,tasks);
+
+                MessageBox.Show("Новий файл успішно створено за шаблоном: " + newFilePath);
+            }
+            else
+            {
+                MessageBox.Show("Студента з введеним прізвищем не знайдено.");
+            }
+
+        }
+
+        static string InsertValuesIntoTemplate(string templateFilePath, string studentName, int labNumber, string[] tasks)
+        {
+            string newFilePath = $"D:\\Diplom\\{studentName} {labNumber}.docx"; // Шлях до нового файлу
+
+            // Копіювання шаблонного файлу
+            System.IO.File.Copy(templateFilePath, newFilePath, true);
+
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(newFilePath, true))
+            {
+                // Зберігаємо всі тексти в окремому списку
+                var texts = doc.MainDocumentPart.Document.Body.Descendants<text>().ToList();
+
+                // Проходимося по копії списку і замінюємо значення
+                foreach (var text in texts)
+                {
+                    // Пошук і заміна відповідних полів
+                    if (text.Text.Contains("fullName"))
+                    {
+                        text.Text = text.Text.Replace("fullName", studentName);
+                    }
+                    else if (text.Text.Contains("LabNumb"))
+                    {
+                        text.Text = text.Text.Replace("LabNumb", labNumber.ToString());
+                    }
+                    else if (text.Text.Contains("first"))
+                    {
+                        text.Text = text.Text.Replace("first", tasks[0]);
+                    }
+                    else if (text.Text.Contains("second"))
+                    {
+                        text.Text = text.Text.Replace("second", tasks[1]);
+                    }
+                    else if (text.Text.Contains("third"))
+                    {
+                        text.Text = text.Text.Replace("third", tasks[2]);
+
+                    }
+                    else if (text.Text.Contains("fourth"))
+                    {
+                        text.Text = text.Text.Replace("fourth", tasks[3]);
+                    }
+                }
+
+                // Зберігаємо зміни у документ
+                doc.MainDocumentPart.Document.Save();
+            }
+            
+
+            return newFilePath;
+
+        }
+
+        // Перевірка, чи є студент з введеним прізвищем в файлі
+        static bool CheckStudentExists(string fullName, string studentsExcelFilePath)
+        {
+            using (var package = new ExcelPackage(new FileInfo(studentsExcelFilePath)))
+            {
+                var worksheet = package.Workbook.Worksheets.FirstOrDefault();
+                if (worksheet != null)
+                {
+                    int rowCount = worksheet.Dimension.Rows;
+
+                    for (int i = 1; i <= rowCount; i++)
+                    {
+                        if (worksheet.Cells[i, 1].Value.ToString() == fullName)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("У файлі немає листів.");
+                }
+            }
+
+            return false;
+        }
+
+        // Метод для перевірки, чи потрібні студенту складні завдання
+        static bool IsComplexTasks(string fullName, string studentsExcelFilePath)
+        {
+            using (var package = new ExcelPackage(new FileInfo(studentsExcelFilePath)))
+            {
+                var worksheet = package.Workbook.Worksheets.FirstOrDefault();
+                if (worksheet != null)
+                {
+                    int rowCount = worksheet.Dimension.Rows;
+
+                    for (int i = 1; i <= rowCount; i++)
+                    {
+                        if (worksheet.Cells[i, 1].Value.ToString() == fullName)
+                        {
+                            string taskType = worksheet.Cells[i, 2].Value.ToString();
+                            return taskType == "+"; // Повертаємо true, якщо студентові потрібні складні завдання
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("У файлі немає листів.");
+                }
+            }
+
+            return false; // Повертаємо false, якщо студентові потрібні прості завдання або не знайдено студента
+        }
+
+        // Метод для вибору завдання для студента
+        static string[] ReadTasks(string tasksTextFilePath)
+        {
+            var lines = File.ReadAllLines(tasksTextFilePath);
+
+            // Перевірка, чи є рядки для читання
+            if (lines.Length == 0)
+            {
+                MessageBox.Show("Файл містить порожню або відсутню інформацію.");
+                return new string[0]; // Повертаємо порожній масив завдань
+            }
+
+
+            // Вибірка завдань з рядків
+            var tasks = new List<string>();
+            foreach (var line in lines)
+            {
+                // Пропускаємо порожні рядки
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+
+                // Додаємо завдання до списку
+                tasks.Add(line.Trim());
+            }
+
+            return tasks.ToArray();
+        }
+
+        static string[] SelectTaskForStudent(string fullName, int labNumber, string tasksTextFilePath)
+        {
+            // Зчитуємо завдання з файлу
+            var tasks = ReadTasks(tasksTextFilePath);
+
+            // Перевіряємо, чи є завдання для вказаної лабораторної роботи
+            if (tasks.Length == 0)
+            {
+                MessageBox.Show($"Не вдалося знайти завдання для лабораторної роботи {labNumber}.");
+                string[] strings = new string[0] ;
+                return strings;
+            }
+
+            // Визначаємо початковий і кінцевий індекси рядків для поточної лабораторної роботи
+            int startLineIndex = Array.IndexOf(tasks, $"Лабораторна робота {labNumber}") + 1;
+            int endLineIndex = tasks.Length;
+            if (labNumber < 3)
+            {
+                endLineIndex = Array.IndexOf(tasks, $"Лабораторна робота {labNumber + 1}");
+            }
+
+            // Створюємо словник для зберігання завдань з кожної групи
+            var groupTasks = new Dictionary<string, List<string>>();
+
+            // Проходимо по рядках з завданнями в межах поточної лабораторної роботи
+            string currentGroup = "";
+            for (int i = startLineIndex; i < endLineIndex; i++)
+            {
+                string line = tasks[i];
+
+                if (line.StartsWith("Група"))
+                {
+                    // Оновлюємо поточну групу
+                    currentGroup = line.Trim();
+                    if (!groupTasks.ContainsKey(currentGroup))
+                    {
+                        groupTasks[currentGroup] = new List<string>();
+                    }
+                }
+                else if (!string.IsNullOrWhiteSpace(line) && !line.StartsWith("Лабораторна робота"))
+                {
+                    // Додаємо завдання до поточної групи, розділяючи їх по комі
+                    var tasksArray = line.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                                          .Select(task => task.Trim())
+                                          .ToList();
+                    groupTasks[currentGroup].AddRange(tasksArray);
+                }
+            }
+
+            // Вибираємо випадкове завдання з кожної групи
+            var random = new Random();
+            var selectedTasks = new List<string>();
+            foreach (var tasksInGroup in groupTasks.Values)
+            {
+                if (tasksInGroup.Count > 0)
+                {
+                    int randomIndex = random.Next(tasksInGroup.Count);
+                    selectedTasks.Add(tasksInGroup[randomIndex]);
+                }
+                else
+                {
+                    MessageBox.Show($"У групі {currentGroup} відсутні завдання.");
+                }
+            }
+
+            // Виводимо обрані випадкові завдання для студента
+            return selectedTasks.ToArray();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
